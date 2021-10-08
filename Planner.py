a7ef55ed7a2c07527f3098b8b6a8f96d27b4e586
Maen Artimy <artimym@gmail.com>
2021-10-08 11:33:54 -0300

    Creating a class

--- Planner.py
+++ Planner.py
@@ -7,44 +7,54 @@ import sys
 import math
 import ipaddress as ipv4
 
-prefix = '10.10.0.0'
-mask = 16
-hosts = [1777, 156, 5, 672, 850, 70]
 
-def check(hosts, mask_len):
-    return sum(hosts) <= 2**(32-mask_len)
-
-def bits_needed(hosts):
-    bits = []
-    for b in hosts:
-        bits.append(math.ceil(math.log2(b+2)))
-    return bits
-
-def net_bits(nets, mask_len):
-    return 32-mask_len-max(nets)
-
-def assign_subnets(prefix, mask_len, ext):
-    x = ipv4.ip_network(f'{prefix}/{mask_len}')
-    return list(x.subnets(ext))
+class AddressPlanner():
     
+    def set_requirements(self, prefix, mask_len, hosts):
+        self.prefix = prefix
+        self.mask_len = mask_len
+        self.hosts = hosts
+        
+        return sum(self.hosts) <= 2**(32-self.mask_len)
     
-if not check(hosts, mask):
-    print("cannot proceed")
-    sys.exit(0)
+    def bits_needed(self):
+        bits = []
+        for b in self.hosts:
+            bits.append(math.ceil(math.log2(b+2)))
+        return bits
     
-
-netbits = bits_needed(hosts)
-
-ext = net_bits(netbits, mask)
-
-num_subnets = 2**ext
-
-if num_subnets < len(hosts):
-    print("you need better algorithm")
-    sys.exit(0)
+    def net_bits(self, nets):
+        return 32-self.mask_len-max(nets)
     
+    def assign_subnets(self):
+        assigned = []       
+        netbits = self.bits_needed()
+        ext = self.net_bits(netbits)
+        num_subnets = 2**ext
+        
+        if num_subnets < len(hosts):
+            print("you need better algorithm")
+            return assigned
+    
+        x = ipv4.ip_network(f'{self.prefix}/{self.mask_len}')
+        subnets = list(x.subnets(ext))
 
-subnets = assign_subnets(prefix, mask, ext)
+        for idx in range(len(hosts)):
+            assigned.append(subnets[idx])
+        return assigned
+    
+if __name__ == "__main__":
 
-for idx, value in enumerate(hosts):
-    print(value, subnets[idx])
+    prefix = '10.10.0.0'
+    mask = 16
+    hosts = [1777, 156, 5, 672, 850, 70]
+
+    planner = AddressPlanner()
+        
+    if not planner.set_requirements(prefix, mask, hosts):
+        print("cannot proceed")
+        sys.exit(0)
+        
+    subnets = planner.assign_subnets()
+    
+    print(subnets)
